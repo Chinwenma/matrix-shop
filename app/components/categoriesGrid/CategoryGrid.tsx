987010d762efb3b25766d4ca15ee1a5175d0fc55
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2026-01-20 15:14:54 +0100

    feat: update CategoriesGrid layout, adjust image dimensions, and set postinstall flag to false in configuration files

--- app/components/categoriesGrid/CategoryGrid.tsx
+++ app/components/categoriesGrid/CategoryGrid.tsx
@@ -10,10 +10,6 @@ export default async function CategoriesGrid() {
   return (
     <section className="py-16 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-10">
-          Explore Our Categories
-        </h2>
-
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {categories.map((category) => (
             <div
@@ -21,18 +17,18 @@ export default async function CategoriesGrid() {
               className="relative group overflow-hidden rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 bg-white"
             >
               {/* Image */}
-              <div className="relative h-64 w-full">
+              <div className="relative">
                 <Image
                   src={category.image!}
                   alt={category.name}
-                  height={700}
-                  width={700}
-                  className="object-cover group-hover:scale-110 transition-transform duration-500"
+                  height={200}
+                  width={200}
+                  className="object-cover w-full group-hover:scale-110 transition-transform duration-500"
                 />
               </div>
 
               {/* Text */}
-              <div className="p-6">
+              <div className="p-12">
                 <h3 className="text-xl font-semibold text-gray-800 mb-2">
                   {category.name}
                 </h3>
